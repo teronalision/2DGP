7381fb4a973e5c9ace84a8c468a932447c7d37bd
teronalision(c) <teronalision@gmail.com>
2018-09-17 17:03:29 +0900

    drill05 틀

--- Drills/Drill-05/drill05.py
+++ Drills/Drill-05/drill05.py
@@ -2,12 +2,24 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_shet.png')
+
+point = ((203, 535),(132, 243),(535, 470),(477,203),(715, 136),(316, 225),(510, 92),(692, 518),(682, 336),(712, 349))
+x, y = 800 //2, 600 //2
+frame = 0
 
 
+def move_to((dx, dy)):
+    pass
+        
 
+def cDraw():
+    pass
+
+#메인
 while True:
-    
+    for dst in point:
+        move_to(dst)
     
     
 close_canvas()

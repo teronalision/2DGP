1fa8c2985c908d6df5c670c5508ef894e4ae4431
teronalision <43131617+teronalision@users.noreply.github.com>
2018-09-17 18:39:58 +0900

    move_to 완성

--- Drills/Drill-05/drill05.py
+++ Drills/Drill-05/drill05.py
@@ -24,12 +24,12 @@ def cDraw(): #그리기
     clear_canvas()
     character.clip_draw(frame*100 ,right*100 ,100 ,100 ,x ,y)
     update_canvas()
-    delay(0.05)
+    delay(0.02)
 
 
 #메인
 while True:
-    for ix, iy in  [(203, 535),(100,100)]:
+    for ix, iy in  [(203, 535),(132, 243),(535, 470),(477, 203),(715, 136),(316, 225),(510, 92),(692, 518),(682, 336),(712, 349)]:
         move_to(ix, iy)
     
     

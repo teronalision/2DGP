d79692c1d6107a2b98641285e6dc36234cce26c3
teronalision(c) <teronalision@gmail.com>
2018-09-17 17:05:11 +0900

    cDraw 완성

--- Drills/Drill-05/drill05.py
+++ Drills/Drill-05/drill05.py
@@ -7,14 +7,17 @@ character = load_image('animation_shet.png')
 point = ((203, 535),(132, 243),(535, 470),(477,203),(715, 136),(316, 225),(510, 92),(692, 518),(682, 336),(712, 349))
 x, y = 800 //2, 600 //2
 frame = 0
+right = True
 
 
 def move_to((dx, dy)):
-    pass
-        
+    while !(x == dx && y == dy):
+        cDraw()
 
 def cDraw():
-    pass
+    clear_canvas()
+    character.clip_draw(frame*100 ,right*100 ,100 ,100 ,x ,y)
+    update_canvas()
 
 #메인
 while True:

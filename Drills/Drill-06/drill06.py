72fa8665b34f98b07f671e4df80782ea4293d091
teronalision(c) <43131617+teronalision@users.noreply.github.com>
2018-10-01 17:07:09 +0900

    캐릭터 추가

--- Drills/Drill-06/drill06.py
+++ Drills/Drill-06/drill06.py
@@ -1,20 +1,25 @@
 import pico2d
 pico2d.open_canvas()
 
-bg = pico2d.load_image('KPU_GROUND.png')
+BG = pico2d.load_image('KPU_GROUND.png')
+charicter = pico2d.load_image('animation_sheet.png')
 
-
-
-
+x, y = 400, 300
+frame = 0
+right = True
 
 def draw():
     pico2d.clear_canvas()
-    bg.draw(0,0)
+    BG.draw(400,300)
+    charicter.clip_draw(frame*100,right*100,100,100,x,y)
+    
     pico2d.update_canvas()
     pico2d.delay(1/30)
 
 
-draw()
 
+
+while True:
+    draw()
 
 pico2d.close_canvas()

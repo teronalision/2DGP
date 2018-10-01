58c5bcd63f4e359c347c5979d18155ae409ab13d
teronalision(c) <43131617+teronalision@users.noreply.github.com>
2018-10-01 17:26:37 +0900

    완성

--- Drills/Drill-06/drill07.py
+++ Drills/Drill-06/drill07.py
@@ -10,21 +10,26 @@ frame = 0
 right = True
 n = 1
 
-points = [(random.randint(-300,-300),random.randint(-300,-300)) for n in range(20)]
+points = [(random.randint(-200,200),random.randint(-200,200)) for n in range(20)]
 
 def draw():
     global frame
     
     pico2d.clear_canvas()
     BG.draw(400,300)
-    charicter.clip_draw(frame*100,right*100,100,100,x,y)
+    charicter.clip_draw(frame*100,right*100,100,100,x+400,y+300)
     
     pico2d.update_canvas()
     pico2d.delay(1/30)
     frame=(frame+1) %8
 
 def move(p1,p2):
-    global x, y, idx
+    global x, y, idx, right
+    if p2[0] - p1[0] < 0:
+        right = False
+    else:
+        right = True
+    
     for i in range(0, 100 +1, 4):
         t = i /100
         x = (1-t)*p1[0] +t*p2[0]
@@ -33,9 +38,8 @@ def move(p1,p2):
         
 
 while True:
-
     
-    move((0,0),(100,100))
+    move(points[n-1],points[n])
     
     n = (n+1) %20
 

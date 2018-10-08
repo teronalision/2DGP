98ec6abbcf8337fbd2108cd2805cbb35f98cfb75
teronalision(c) <>
2018-10-08 16:47:47 +0900

    초기

--- Drills/Drill-09/boy_grass_object.py
+++ Drills/Drill-09/boy_grass_object.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import random
 
 # Game object class here
 
@@ -12,18 +12,51 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame +1) %8
+        self.x +=5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
 # initialization code
 running = True
 open_canvas()
 
-boy = Boy()
+team = [Boy() for i in range(11)]
 grass = Grass()
 
 # game main loop code
 
 while running:
     handle_events()
-    delay(1)
-close_canvas()
+
+    for boy in team:
+        boy.update()
+
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+
+    delay(0.05)
+
 
 # finalization code
+
+close_canvas()

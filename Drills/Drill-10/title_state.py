1a7167766b67e09f51f8439bd66ee1513a88742f
teronalision(c) <>
2018-10-11 16:56:48 +0900

    메인까지 연결

--- Drills/Drill-10/title_state.py
+++ Drills/Drill-10/title_state.py
@@ -1,4 +1,5 @@
 import game_framework
+import main_state
 from pico2d import *
 
 
@@ -7,19 +8,32 @@ image = None
 
 
 def enter():
-    pass
+    global image
+    image = load_image('title.png')
 
 
 def exit():
-    pass
+    global image
+    del(image)
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
+
 
 
 def draw():
-    pass
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
 
 
 

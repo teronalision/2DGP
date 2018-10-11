71ad6b768cf8531e19d42ab45ae131b034fe33fe
teronalision(c) <>
2018-10-11 17:12:35 +0900

    일시정지 뼈대

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,33 @@
+import game_farmework
+import main_state
+from pico2d import *
+
+
+name = "PauseState"
+image = None
+
+
+def enter():
+    global image
+    image = load_image('pause.png')
+
+
+def exit():
+    global image
+    del(image)
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_Q):
+                game_framework.quit()
+
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
--- Drills/Drill-10/start_state.py
+++ Drills/Drill-10/start_state.py
@@ -23,7 +23,7 @@ def update():
 
     if logo_time > 1.0:
         logo_time = 0
-        game_framework.change_state(title_state)
+        game_framework.push_state(title_state)
     delay(0.01)
     logo_time += 0.01
 

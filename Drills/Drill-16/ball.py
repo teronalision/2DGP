240eb04408cae527dc6cacb207ec78a93060562e
teronalision(c) <43131617+teronalision@users.noreply.github.com>
2018-11-22 16:45:53 +0900

    공 y 랜덤

--- Drills/Drill-16/ball.py
+++ Drills/Drill-16/ball.py
@@ -9,7 +9,7 @@ class Ball:
     def __init__(self,bg):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
+        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), random.randint(0, 600-1), 0
         self.bg = bg
 
     def get_bb(self):
@@ -18,7 +18,7 @@ class Ball:
     def draw(self):
         cx, cy = self.x - self.bg.window_left, self.y - self.bg.window_bottom
         self.image.draw(cx,cy)
-        draw_rectangle(*self.get_bb())
+        #draw_rectangle(*self.get_bb())
 
     def update(self):
         self.y -= self.fall_speed * game_framework.frame_time
